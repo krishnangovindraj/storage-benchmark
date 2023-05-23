aaf02722cdc1def0b398737ede300ff685413b81
Krishnan Govindraj <krishnan@vaticle.com>
2023-05-23 11:59:37 +0100

    Prefix hash to attributes to disturb sorted-ness of inserts; Add some attributes

--- simulation/PersonAgent.kt
+++ simulation/PersonAgent.kt
@@ -24,6 +24,8 @@ import com.vaticle.typedb.simulation.common.seed.RandomSource
 import com.vaticle.typedb.simulation.typedb.TypeDBClient
 
 import com.vaticle.typeql.lang.TypeQL
+import java.time.LocalDateTime
+import java.util.*
 
 public class PersonAgent(client: TypeDBClient, context: Context) :
     Agent<Context.DBPartition, TypeDBSession, Context>(client, context) {
@@ -31,8 +33,22 @@ public class PersonAgent(client: TypeDBClient, context: Context) :
     override val agentClass = PersonAgent::class.java
     override val partitions = context.partitions
 
+    val timeZero: LocalDateTime = LocalDateTime.now().withNano(0);
+
     private fun nameFrom(partitionId: Int, id: Int): String {
-        return "name" + partitionId + ":" + id
+        return Objects.hash("name", partitionId, id).toString() + "-" + partitionId + ":" + id
+    }
+
+    private fun ssidFrom(partitionId: Int, id: Int): Long {
+        return Objects.hash("ssid", partitionId, id).toLong()
+    }
+
+    private fun addressFrom(partitionId: Int, id: Int): String {
+        return Objects.hash("address", partitionId, id).toString() + "-" + partitionId + ":" + id
+    }
+
+    private fun dateFrom(first: Int, second: Int): LocalDateTime {
+        return timeZero.plusSeconds(Objects.hash("date", first, second).toLong())
     }
 
     fun createPerson(
@@ -42,9 +58,14 @@ public class PersonAgent(client: TypeDBClient, context: Context) :
     ): List<Agent.Report> {
         session.transaction(TypeDBTransaction.Type.WRITE).use { tx ->
             for (i in 1..context.model.personPerBatch) {
-                val name : String = nameFrom(dbPartition.partitionId, dbPartition.idCtr.addAndGet(1))
+                val id: Int = dbPartition.idCtr.addAndGet(1)
                 tx.query().insert(TypeQL.insert(
-                    TypeQL.`var`("p").isa("person").has("name", name)))
+                    TypeQL.`var`("p").isa("person")
+                        .has("name", nameFrom(dbPartition.partitionId, id))
+                        .has("ssid", ssidFrom(dbPartition.partitionId, id))
+                        .has("address", addressFrom(dbPartition.partitionId, id))
+
+                ))
             }
             tx.commit()
         }
@@ -58,13 +79,14 @@ public class PersonAgent(client: TypeDBClient, context: Context) :
     ): List<Agent.Report> {
         session.transaction(TypeDBTransaction.Type.WRITE).use { tx ->
             for (i in 1..context.model.friendshipPerBatch) {
-                val first : String = nameFrom(dbPartition.partitionId, randomSource.nextInt(dbPartition.idCtr.get()))
-                val second : String = nameFrom(dbPartition.partitionId, randomSource.nextInt(dbPartition.idCtr.get()))
+                val first : Int = 1 + randomSource.nextInt(dbPartition.idCtr.get())
+                val second : Int = 1 + randomSource.nextInt(dbPartition.idCtr.get())
                 tx.query().insert(TypeQL.match(
-                    TypeQL.`var`("p1").isa("person").has("name", first),
-                    TypeQL.`var`("p2").isa("person").has("name", second),
+                    TypeQL.`var`("p1").isa("person").has("name", nameFrom(dbPartition.partitionId, first)),
+                    TypeQL.`var`("p2").isa("person").has("name", nameFrom(dbPartition.partitionId, second)),
                     ).insert(
                         TypeQL.rel("person","p1").rel("person","p1").isa("friendship")
+                            .has("meeting-time", dateFrom(first, second))
                     ))
             }
             tx.commit()
